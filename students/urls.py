9e3a9bf476f95c13b9a764a132fb6828f139d789
Abhishek-BG <abhigowda0190@gmail.com>
2025-06-10 10:01:11 +0530

    updates

--- students/urls.py
+++ students/urls.py
@@ -4,6 +4,4 @@ urlpatterns =[
     path("",views.home, name="home"),
     path("login/",views.StudentLogin, name="login"),
     path("dashboard/",views.StudentDashBoard, name="dash")
-    #http://127.0.0.1:8000/student/ 
-    #https://github.com/Abhishek-BG/mcaprojs
 ]
--- students/views.py
+++ students/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 
 def home(request): #controller 
-    return HttpResponse("<h1>Welcome to Students Home Page</h1>")
+    return render(request,'students/home.html')
 
 def StudentLogin(request):
-    return HttpResponse(" <h1>Login Page</h1>")
+    return render(request,'students/login.html')
+    #return render(request,templatefile name)
 
 def StudentDashBoard(request):
     return HttpResponse(" <h1>Your Profile</h1>")
