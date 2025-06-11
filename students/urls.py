49fab13efa44fbf3029dd6fe7ad397e70defcca0
Abhishek-BG <abhigowda0190@gmail.com>
2025-06-11 15:08:27 +0530

    up

--- students/urls.py
+++ students/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns =[
     path("",views.home, name="home"),
     path("login/",views.StudentLogin, name="login"),
-    path("dashboard/",views.StudentDashBoard, name="dash")
+    path("dashboard/",views.StudentDashBoard, name="dash"),
+    path("create/",views.student_create,name="student_create")
 ]
--- students/views.py
+++ students/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpRequest,HttpResponse
 from .models import Student
-
-
+from .forms import StudentForm
 pages= [{'name':'Home Page','url':'home'},
         {'name':'Login Page','url':'login'}
 ]
+#create 
+def student_create(req):
+    if req.method=="POST":
+        form = StudentForm(req.POST) #object mapping
+        if form.is_valid(): #
+            form.save() # saved to DB
+            return redirect('home') #home page
+    else:
+        form = StudentForm()
+    return render(req,'students/student_create.html',
+                    {'form':form}
+                    )
+
+
+
+
+#read all
 def home(request): #controller  #http://127.0.0.1:8000/students/
     #all the students details 
     students = Student.objects.all();
@@ -13,9 +29,9 @@ def home(request): #controller  #http://127.0.0.1:8000/students/
                   context={'students':students,
                            'items':pages})
 def StudentLogin(request):
-    return render(request,'students/login.html'
-                  ,context={'items':pages})
-    #return render(request,templatefile name)
+    return HttpResponse(" <h1>Login</h1>")
 
+    print(request)
+    return response
 def StudentDashBoard(request):
     return HttpResponse(" <h1>Your Profile</h1>")
