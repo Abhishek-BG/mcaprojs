4d11b2fdb2cce3fe24bad7baaa5979895eca4cd4
Abhishek-BG <abhigowda0190@gmail.com>
2025-06-11 13:57:13 +0530

    updates

--- students/views.py
+++ students/views.py
@@ -2,20 +2,16 @@ from django.shortcuts import render
 from django.http import HttpRequest,HttpResponse
 from .models import Student
 
-people = [
-    {'name':'raju','age':20,'active':True,'per':98},
-    {'name':'chotta bheem','age':29,'active':True,'per':93},
-    {'name':'mukesh','age':22,'active':False,'per':96},
-    {'name':'kirmada','age':20,'active':True,'per':90},
-    ]
+
 pages= [{'name':'Home Page','url':'home'},
         {'name':'Login Page','url':'login'}
 ]
 def home(request): #controller  #http://127.0.0.1:8000/students/
+    #all the students details 
+    students = Student.objects.all();
     return render(request,'students/home.html',
-                  context={'peoples':people,
+                  context={'students':students,
                            'items':pages})
-
 def StudentLogin(request):
     return render(request,'students/login.html'
                   ,context={'items':pages})
